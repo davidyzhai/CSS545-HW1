db23481fee3e3b957dc2e36d0523713e5ca6af7a
David Zhai <102549245+davidyzhai@users.noreply.github.com>
2024-04-06 23:45:37 -0700

    Add files via upload

---
+++ AnotherActivity.kt
@@ -0,0 +1,39 @@
+package com.example.navigateoneactivitytoanother
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.navigateoneactivitytoanother.ui.theme.NavigateOneActivityToAnotherTheme
+
+class AnotherActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(onClick = {val navigate = Intent(this@AnotherActivity, MainActivity::class.java)
+                    startActivity(navigate)
+                }) {
+                    Text(text = "Navigate Back", fontSize = 10.sp)
+                }
+            }
+        }
+    }
+}
+
